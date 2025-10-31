43072dfb24b2089c8438d6d696ef7f33307519cc
Van Dat <vandat0709.se@gmail.com>
2025-10-31 23:53:31 +0700

    video 19

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  const configService = app.get(ConfigService);
 
-  await app.listen(8080);
+  const port = configService.get('PORT');
+  await app.listen(port);
 }
 bootstrap();
